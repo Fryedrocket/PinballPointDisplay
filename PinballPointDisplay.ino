f7b6c7346b754a8018a5718e979ef713ece7ae65
Fryedrocket <76858907+Fryedrocket@users.noreply.github.com>
2023-03-21 11:37:23 -0700

    Update PinballPointDisplay.ino

--- PinballPointDisplay.ino
+++ PinballPointDisplay.ino
@@ -3,11 +3,9 @@ int LatchPin = 9;
 int ClockPin = 10;
 int NumberOfDisplays = 3;
 
-#include "Edgedetection.h"
-
 #define sol1 4
 #define sol2 6
-#define sol3 8
+#define sol3 13
 
 
 byte nums[] = { B01110111,
@@ -36,14 +34,16 @@ byte _9 = B11110110;
 
 uint32_t count = 0;
 
-int s1, s2;
+int s1, s2, s3;
 
-bool laststate1 = false;  // false = low; high = true;
-bool laststate2 = false;  // false = low; high = true;
+bool laststate1, laststate2, laststate3;  // false = low; high = true;
 
 
-void activateSolenoid(uint8_t triggerPin, uint8_t solenoidPin) {
-  if (triggerPin == HIGH) {
+void activateSolenoid(uint8_t triggerPin, uint8_t solenoidPin, bool override) {
+  if (override == true) {
+    digitalWrite(solenoidPin, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else if (triggerPin == HIGH) {
     digitalWrite(solenoidPin, HIGH);
     digitalWrite(LED_BUILTIN, HIGH);
   } else {
@@ -56,6 +56,8 @@ void setup() {
   pinMode(4, OUTPUT);
   pinMode(5, INPUT);
   pinMode(6, OUTPUT);
+  pinMode(11, INPUT);
+  pinMode(13, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   pinMode(LatchPin, OUTPUT);
@@ -71,9 +73,11 @@ void setup() {
 void loop() {
   s1 = digitalRead(3);
   s2 = digitalRead(5);
+  s3 = digitalRead(11);
 
-  activateSolenoid(s1, sol1);
-  activateSolenoid(s2, sol2);
+  activateSolenoid(s1, sol1, false);
+  activateSolenoid(s2, sol2, false);
+  activateSolenoid(s3, sol3, false);
   if (s1 == HIGH && s1 != laststate1) {
     count++;
     shiftNum(count);
@@ -82,9 +86,14 @@ void loop() {
     count++;
     shiftNum(count);
   }
+  if (s3 == HIGH && s3 != laststate3) {
+    count++;
+    shiftNum(count);
+  }
 
   laststate1 = s1;
   laststate2 = s2;
+  laststate3 = s3;
 }
 void shiftNum(uint32_t num) {
   digitalWrite(LatchPin, LOW);
